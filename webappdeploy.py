8dd48f3b419eec794ca40c32f5c2755278a29596
Ravikishan <ravikishan1996@gmail.com>
2024-07-22 14:08:27 +0530

    1st done

--- webappdeploy.py
+++ webappdeploy.py
@@ -9,34 +9,34 @@ import os
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
-# def create_bucket(bucket_name, region=None):
-#     try:
-#         if region is None:
-#             s3_client = boto3.client('s3')
-#             s3_client.create_bucket(Bucket=bucket_name)
-#         else:
-#             s3_client = boto3.client('s3', region_name=region)
-#             location = {'LocationConstraint': region}
-#             s3_client.create_bucket(Bucket=bucket_name,CreateBucketConfiguration=location)
-#     except ClientError as e:
-#         logging.error(e)
-#         return False
-#     return True
+def create_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
-# def upload_file(file_name, bucket, object_name=None):
+def upload_file(file_name, bucket, object_name=None):
 
-#     if object_name is None:
-#         object_name = os.path.basename(file_name)
+    if object_name is None:
+        object_name = os.path.basename(file_name)
 
-#     # Upload the file
-#     s3_client = boto3.client('s3')
-#     try:
-#         response = s3_client.upload_file(file_name, bucket, object_name)
-#     except ClientError as e:
-#         logging.error(e)
-#         return False
-#     return True
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 def create_ec2_instance():
     
@@ -72,6 +72,6 @@ def create_ec2_instance():
 
 
 if __name__=="__main__":
-    # create_bucket(bucket_name="rakshi2107", region= "ap-northeast-2")
-    # upload_file(file_name=r"C:\Users\Ravik\Pictures\Screenshots\containarize.png",bucket="rakshi2107",object_name=None)
+    create_bucket(bucket_name="rakshi2107", region= "ap-northeast-2")
+    upload_file(file_name=r"C:\Users\Ravik\Pictures\Screenshots\containarize.png",bucket="rakshi2107",object_name=None)
     create_ec2_instance()
