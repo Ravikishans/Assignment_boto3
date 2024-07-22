b21f521f405be66788f8cebffeab8b19c724db7f
Ravikishan <ravikishan1996@gmail.com>
2024-07-22 14:07:53 +0530

    1st done

--- webappdeploy.py
+++ webappdeploy.py
@@ -9,36 +9,69 @@ import os
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
-def create_bucket(bucket_name, region=None):
-    try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,CreateBucketConfiguration=location)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+# def create_bucket(bucket_name, region=None):
+#     try:
+#         if region is None:
+#             s3_client = boto3.client('s3')
+#             s3_client.create_bucket(Bucket=bucket_name)
+#         else:
+#             s3_client = boto3.client('s3', region_name=region)
+#             location = {'LocationConstraint': region}
+#             s3_client.create_bucket(Bucket=bucket_name,CreateBucketConfiguration=location)
+#     except ClientError as e:
+#         logging.error(e)
+#         return False
+#     return True
 
 
-def upload_file(file_name, bucket, object_name=None):
+# def upload_file(file_name, bucket, object_name=None):
 
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+#     if object_name is None:
+#         object_name = os.path.basename(file_name)
 
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+#     # Upload the file
+#     s3_client = boto3.client('s3')
+#     try:
+#         response = s3_client.upload_file(file_name, bucket, object_name)
+#     except ClientError as e:
+#         logging.error(e)
+#         return False
+#     return True
+
+def create_ec2_instance():
+    
+    ec2= boto3.client("ec2")
+    instance_params = {
+        'ImageId': 'ami-056a29f2eddc40520',  # Replace with a valid image ID
+        'InstanceType': 't3.micro',
+        'MinCount': 1,
+        'MaxCount': 1,
+        'KeyName': 'raviAWS',  # Replace with your key pair name
+        # 'SecurityGroupIds': ['Default'],  # Replace with your security group ID
+        # 'SubnetId': 'Default',  # Replace with your subnet ID
+        'TagSpecifications': [{
+            'ResourceType': 'instance',
+            'Tags': [{
+                'Key': 'Name',
+                'Value': 'RaviScaling'
+            }]
+        }],
+        'UserData': '''#!/bin/bash
+            sudo apt update -y
+            sudo apt install nginx -y
+            sudo service nginx start
+            sudo service nginx enable
+        '''
+    }
+    instance = ec2.run_instances(**instance_params)
+    
+    instance_id = instance['Instances'][0]['InstanceId']
+    print(f'EC2 instance created with ID: {instance_id}')
+    
+    return instance_id
 
 
 if __name__=="__main__":
-    create_bucket(bucket_name="rakshi2107", region= "ap-northeast-2")
-    upload_file(file_name=r"C:\Users\Ravik\Pictures\Screenshots\containarize.png",bucket="rakshi2107",object_name=None)
+    # create_bucket(bucket_name="rakshi2107", region= "ap-northeast-2")
+    # upload_file(file_name=r"C:\Users\Ravik\Pictures\Screenshots\containarize.png",bucket="rakshi2107",object_name=None)
+    create_ec2_instance()
